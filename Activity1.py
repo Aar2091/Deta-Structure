946b3a2e1401d20eb1de9ead00f150fcf17ed3d7
Aar2091 <spheres-turbine-4t@icloud.com>
2025-06-26 16:50:57 +0530

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triples = [1, 2, 3] * 3
+print(triples)
+
+alist = [100, 200, 300, 400, 50]
+alist = alist[::-1]
+print(alist,"\n")
+
+print(numbers[3])
+
+print(numbers[2:4])
+
+
