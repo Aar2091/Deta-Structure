de356766fb3ef936ff8cc7a9ad5d6186aeb39180
Aar2091 <spheres-turbine-4t@icloud.com>
2025-06-29 11:38:31 +0530

    Add files via upload

---
+++ Opperations.py
@@ -0,0 +1,25 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+print()
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+print()
+
+tuplex = tuplex + (9,)
+print(tuplex)
+print()
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuplex)
+print()
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+print()
+
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+_slice = tuplex[3:5]
+print(slice)
+print()
+
